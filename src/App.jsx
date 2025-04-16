6723932a42ebc23efb93eb5bfe690d7e8953866e
Osip <52411274+Osip-d25@users.noreply.github.com>
2025-04-16 11:48:16 +0300

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -34,15 +34,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    {/*    script     */}
+
     </>
   )
 }
